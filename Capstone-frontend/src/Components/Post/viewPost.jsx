0616cd73d122c83b2052ba0968dc1d39134dbd69
Zachary <taylorzw96@gmail.com>
2021-08-28 17:20:47 -0500

    Put specific post context into a react bootstrap card

--- Capstone-frontend/src/Components/Post/viewPost.jsx
+++ Capstone-frontend/src/Components/Post/viewPost.jsx
@@ -1,35 +1,42 @@
 import React from "react";
-import { Col, Container, Row, InputGroup } from "react-bootstrap";
+import { Col, Container, Row, InputGroup, Card } from "react-bootstrap";
+import ConnectNavigationBar from "../Connect/connectNavBar";
 
 const ViewPost = (props) => {
     const {postContext} = props.currentPost
   return (
     <React.Fragment>
-      <div className="orbit">
-      <Container>
-        <Row>
-        <Col sm={2}></Col>
-          <Col className="d-flex justify-content-center" sm={4}>
-            <div>
-                <h1>{postContext}</h1>
-            </div>
-            </Col>
-            <Col className="commentbar" sm={4}>
-          <InputGroup className="mb-3">
-            {/* <InputGroup.Text id="basic-addon1"></InputGroup.Text> */}
-            <input
-            placeholder="Comment..."
-            aria-label="Comment..."
-            aria-describedby="basic-addon1"
-            />
-            </InputGroup>
-          </Col>
-          <Col sm={2}></Col>
-        </Row>
-        </Container>
-      </div>
+        <ConnectNavigationBar />
+        <Container>
+            <Row>
+                <Col sm={4}></Col>
+                <Col sm={8}></Col>
+            </Row>
+            </Container>
+            <Container fluid>
+                <Row className="d-flex justify-content-center">
+                        <Card
+                        className="card-container border border-primary "
+                        style={{ width: "18rem", margin: "1rem" }}
+                        >
+                        <Card.Body className="text-center">
+                            <Card.Text>{postContext}</Card.Text>
+                            <div className="mb-2"></div>
+                            <InputGroup className="mb-3">
+                                {/* <InputGroup.Text id="basic-addon1"></InputGroup.Text> */}
+                                <input
+                                placeholder="Comment..."
+                                aria-label="Comment..."
+                                aria-describedby="basic-addon1"
+                                />
+                            </InputGroup>
+                        </Card.Body>
+                    </Card>
+                </Row>
+            </Container>
     </React.Fragment>
   );
 };
 
 export default ViewPost;
+
