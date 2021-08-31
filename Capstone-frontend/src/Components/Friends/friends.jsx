678247574f3859277fa4642a1f7cefe9c1dbbc6d
Zachary <taylorzw96@gmail.com>
2021-08-31 01:05:52 -0500

    Friends list added

--- Capstone-frontend/src/Components/Friends/friends.jsx
+++ Capstone-frontend/src/Components/Friends/friends.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import FriendSuggestions from './friendsSuggestions';
 import ConnectNavigationBar from '../Connect/connectNavBar';
+import { Container, Row, Col, Button, Form, Card } from 'react-bootstrap';
 
-const Friends = () => {
+function Friends() {
+    const friends = ["Zidan Hunter", "Iwan Hess", "Cameron Ridley", "Zak Villarreal"]
+
     return ( 
         <React.Fragment>
             <ConnectNavigationBar />
+            <Container>
+                    <Row>
+                        <Col sm={8}></Col>
+                        <Col sm={4}></Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col sm={4}></Col>
+                        <Col sm={4}>
+                            <div className="friends">
+                                <h1 className="title mb-3">Friends</h1>
+                            </div>
+                            {friends.map((friendsList) => (
+                            <Card
+                                className="card-container border border-primary "
+                                style={{ display:"flex", width: "21rem", margin: "0rem" }}
+                            >
+                            <Card.Body className="text-center">
+                                <Card.Text>{friendsList}</Card.Text>
+                            </Card.Body>
+                            </Card>
+                            ))}
+                        </Col>
+                        <Col sm={4}></Col>
+                    </Row>
+                </Container>
             <FriendSuggestions />
         </React.Fragment>
      );
